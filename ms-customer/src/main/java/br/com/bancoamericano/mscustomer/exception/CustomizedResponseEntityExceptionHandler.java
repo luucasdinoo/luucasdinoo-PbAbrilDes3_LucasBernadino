4805350dcaa246efa914e8d0e9961bfae4a6de8a
Lucas Bernadino <luucasdinoo@gmail.com>
2024-06-27 22:28:48 -0300

    Customizing some exceptions

---
+++ ms-customer/src/main/java/br/com/bancoamericano/mscustomer/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,43 @@
+package br.com.bancoamericano.mscustomer.exception;
+
+import br.com.bancoamericano.mscustomer.exception.exceptions.CustomerNotFoundException;
+import br.com.bancoamericano.mscustomer.exception.exceptions.RequiredObjectIsNullException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleGenericExceptions(CustomerNotFoundException ex, WebRequest request) {
+        var response = new ExceptionResponse(new Date(),
+                ex.getMessage(),
+                request.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RequiredObjectIsNullException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(RequiredObjectIsNullException ex, WebRequest request) {
+        var response = new ExceptionResponse(new Date(),
+                ex.getMessage(),
+                request.getDescription(false),
+                HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(CustomerNotFoundException ex, WebRequest request) {
+        var response = new ExceptionResponse(new Date(),
+                ex.getMessage(),
+                request.getDescription(false),
+                HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ ms-customer/src/main/java/br/com/bancoamericano/mscustomer/exception/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.bancoamericano.mscustomer.exception.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
