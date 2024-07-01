a69fed4fefa4ee79b2e885de3f4ddd1fe1357596
Lucas Bernadino <luucasdinoo@gmail.com>
2024-07-01 11:29:00 -0300

    Criação do projeto ms-calculate

---
+++ ms-calculate/src/main/java/br/com/bancoamericano/mscalculate/MsCalculateApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancoamericano.mscalculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCalculateApplication.class, args);
+	}
+
+}
---
+++ ms-calculate/src/test/java/br/com/bancoamericano/mscalculate/MsCalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bancoamericano.mscalculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCalculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
